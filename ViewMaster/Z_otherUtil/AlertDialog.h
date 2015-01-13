d772509d70acc8de576ea67064705901ed59795f
HamanyanDev <hi_ro_0219@yahoo.co.jp>
2015-01-13 23:53:59 +0900

    renew_0113

---
+++ ViewMaster/Z_otherUtil/AlertDialog.h
@@ -0,0 +1,14 @@
+//
+//  AlertDialog.h
+//  ViewMaster
+//
+//  Created by bearbrick on 2014/10/05.
+//  Copyright (c) 2014年 HiroyukiHamana. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AlertDialogg : NSObject
+
+
+@end
